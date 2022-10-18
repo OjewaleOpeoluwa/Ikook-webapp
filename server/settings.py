d71839b9ce7e6217d64ab89b8a77c5862abfdd3b
DavidsonCo <opeoluwa313@gmail.com>
2022-10-18 14:58:39 +0100

    Added Steps

--- server/settings.py
+++ server/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -203,5 +203,6 @@ AUTHENTICATION_BACKENDS = (
 #         }
 #     }
 # }
+SITE_ID = 1
 
 django_heroku.settings(locals())
