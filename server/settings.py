3e3e80b280419b6476e9250bc6ccf486c25fd92c
DavidsonCo <opeoluwa313@gmail.com>
2022-10-24 15:32:59 +0100

    Changed CORS Policy

--- server/settings.py
+++ server/settings.py
@@ -96,7 +96,7 @@ WSGI_APPLICATION = "server.wsgi.application"
 # CORS_ALLOWED_ORIGINS = [
 #     'http://localhost:3000',
 # ]
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
